739059350b6bc59dd17ce91433bd2316f458302f
xyzrlee <cnrickylee@gmail.com>
2022-06-21 17:25:43 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e
+
 mkdir -p /data /logs
 
 if [ ! -f ${HONEYPOT_SSH_KEY_FILE} ]
 then
-    ssh-genkey -t rsa -f ${HONEYPOT_SSH_KEY_FILE} -q -N ""
+    ssh-keygen -t rsa -f ${HONEYPOT_SSH_KEY_FILE} -q -N ""
 fi
 
 ls -l ${HONEYPOT_SSH_KEY_FILE}
